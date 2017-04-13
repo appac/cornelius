0017be584819a5c357e1f672d203a3d998b7af3f
Chris Apparicio <chris@apparicio.me>
2017-04-14 00:55:30 +0100

    getPlayer functionality

--- index.js
+++ index.js
@@ -54,10 +54,74 @@ cornelius.prototype.findPlayer = function (playerName, pruned) {
 };
 
 cornelius.prototype.getPlayer = function (playerName, givenKey) {
+		return new Promise(function (resolve, reject) {
+			var uri = url.parse(baseUrl + '/named.search_player_all.bam?sport_code=\'mlb\'&name_part=\'' + playerName + '%25\'&active_sw=\'Y\'');
 
+			var options = {
+				host: uri.host,
+				path: uri.path
+			};
+
+			http.get(options, function (res) {
+				const statusCode = res.statusCode;
+				const contentType = res.headers['content-type'];
+
+				let error;
+
+				if (statusCode !== 200) {
+					error = new Error(`${statusCode} - Request failed.`);
+				} else if (!/^application\/json/.test(contentType)) {
+					error = new Error(`Invalid content type received. Expected JSON, got ${contentType}`);
+				}
+				if (error) {
+					res.resume();
+					reject(error.message);
+				}
+
+				res.setEncoding('utf8');
+				let rawData = '';
+				res.on('data', (chunk) => { rawData += chunk; });
+				res.on('end', () => {
+					try {
+						const parsedData = JSON.parse(rawData);
+						const player = findPlayerInResults(parsedData, givenKey);
+						resolve(player);
+					} catch (e) {
+						reject(e.message)
+					}
+				});
+			}).on('error', (e) => {
+				reject(e.message);
+			});
+
+	});
 };
 
 module.exports = new cornelius();
+
+function findPlayerInResults(data, key) {
+	var resultsCount = data.search_player_all.queryResults.totalSize;
+	var requestedPlayer = {};
+	var key = key.toUpperCase();
+
+	function hasMatchingKey(player) {
+		if (player.team_abbrev === key || player.player_id === key) {
+			return true;
+		}
+		return false;
+	}
+
+	if (resultsCount > 1) {
+		var results = data.search_player_all.queryResults.row;
+		var p = results.findIndex(hasMatchingKey);
+		requestedPlayer = results[p];
+	}
+
+	return requestedPlayer
+	
+}
+
+
 
 function pruneData(data) {
 	var prunedData = [];
