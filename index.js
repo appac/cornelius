75273458e29bf34131a1c8753b90432c039cbdb3
Chris Apparicio <chris@apparicio.me>
2017-04-20 21:49:55 +0100

    add pruning for search results and player objects

--- index.js
+++ index.js
@@ -115,7 +115,88 @@ cornelius.prototype.getHistoric = function (query, key) {
 	});
 }
 
+cornelius.prototype.prune = function (data) {
+	let isPlayerData = data.hasOwnProperty('player_id');
+	let isSearchResults = data.hasOwnProperty('search_player_all');
+	if (isPlayerData) {
+		return prunePlayerData(data);
+	} else if (isSearchResults) {
+		return pruneSearchResults(data);
+	} else {
+		return new Error('Invalid data given to prune.');
+	}
+
+}
+
 module.exports = new cornelius;
+
+function prunePlayerData(data) {
+	let player = {
+		id: data.player_id,
+		name: {
+			full: data.name_display_first_last,
+			first: data.name_first,
+			last: data.name_last,
+			roster: data.name_display_roster
+		},
+		position: {
+			id: data.position_id,
+			code: data.position
+		},
+		team: {
+			id: data.team_id,
+			name: data.team_full,
+			abbrev: data.team_abbrev,
+			code: data.team_code,
+			league: data.league
+		},
+		date: {
+			pro_debut: data.pro_debut_date,
+			birth: data.birth_date
+		},
+		geo: {
+			city: data.birth_city,
+			state: data.birth_city,
+			country: data.birth_country,
+			high_school: data.high_school,
+			college: data.college
+		},
+		attribute: {
+			bats: data.bats,
+			throws: data.throws,
+			weight: data.weight,
+			height: {
+				feet: data.height_feet,
+				inches: data.height_inches
+			}
+		}
+	};
+
+	return player;
+
+}
+
+function pruneSearchResults(data) {
+	let resultsCount = data.search_player_all.queryResults.totalSize;
+	let prunedResults = [];
+
+	if (resultsCount > 1) {
+		let searchResults = data.search_player_all.queryResults.row;
+
+		searchResults.forEach(function (player) {
+			let prunedPlayer = prunePlayerData(player);
+			prunedResults.push(prunedPlayer);
+		});
+	} else {
+		let player = data.search_player_all.queryResults.row;
+		let prunedPlayer = prunePlayerData(player);
+
+		prunedResults.push(prunedPlayer);
+	}
+
+	return prunedResults;
+
+}
 
 function findPlayerInResults(mlbData, key) {
 	if (!key) {
