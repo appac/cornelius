b3c2c0bd38abeb28fe63b7a19923e9fe6852dd7e
Chris Apparicio <chris@apparicio.me>
2017-04-24 14:03:36 +0100

    correct reference to prune.playerData within prune.searchData
    switched forEach to regular for loop, to maintain context

--- utils/prune/index.js
+++ utils/prune/index.js
@@ -3,17 +3,18 @@ let prune = function () {}
 prune.prototype.searchResults = function (data) {
 	let resultsCount = data.search_player_all.queryResults.totalSize;
 	let prunedResults = [];
-
+	
 	if (resultsCount > 1) {
 		let searchResults = data.search_player_all.queryResults.row;
 
-		searchResults.forEach(function (player) {
-			let prunedPlayer = prunePlayerData(player);
+		for (let i = 0; i < searchResults.length; i++) {
+			let player = searchResults[i];
+			let prunedPlayer = this.playerData(player);
 			prunedResults.push(prunedPlayer);
-		});
+		}
 	} else {
 		let player = data.search_player_all.queryResults.row;
-		let prunedPlayer = prunePlayerData(player);
+		let prunedPlayer = this.playerData(player);
 
 		prunedResults.push(prunedPlayer);
 	}
