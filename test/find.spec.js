2992df958a25375301e0ab4d0a274dbb244c634f
Chris Apparicio <chris@apparicio.me>
2017-04-24 16:20:02 +0100

    tests moved to separate files
    based on context

---
+++ test/find.spec.js
@@ -0,0 +1,56 @@
+const chai = require('chai'),
+			expect = chai.expect
+			find = require('../utils/find');
+
+let testData = require('./mock/search.json');
+
+describe('find', function () {
+
+	describe('player', function () {
+
+		it('should find and return the correct player when given a player ID', function () {
+			// Looking for Eric Young Jr. of the New York Yankees - 458913
+			let requestedPlayer = find.player(testData, '458913');
+			expect(requestedPlayer).to.have.property('player_id', '458913');
+		});
+		it('should return the correct player when given a team abbreviation', function () {
+			// Looking for Chris Young of the Kansas City Royals - 432934
+			let requestedPlayer = find.player(testData, 'KC');
+			expect(requestedPlayer).to.have.property('player_id', '432934');
+		});
+
+		describe('error handling', function () {
+			it('should return missing key error if no key is given', function() {
+				let requestedPlayer = find.player(testData, '');
+				expect(requestedPlayer).to.be.an('error', 'find.player wasn\'t given a key');
+			});
+			it('should return an invalid key type error if an invalid key is given', function () {
+				let requestedPlayer = find.player(testData, 123456);
+				expect(requestedPlayer).to.be.an('error', 'find.player expected key to be a string, but was given a number.')
+			});
+			it('should return an error if no matching key is found', function () {
+				let requestedPlayer = find.player(testData, '123456');
+				expect(requestedPlayer).to.be.an('error', 'find.player could not find a player with a matching key.');
+			});
+		});
+
+	});
+
+	describe('teamId', function () {
+		// Find team ID of New York Mets - 121
+		it ('should find and return the correct team ID when given a team abbreviation', function () {
+			let teamId = find.teamId('nym');
+			expect(teamId).to.equal('121');
+		});
+		it ('should find and return the correct team ID when given a team name', function () {
+			let teamId = find.teamId('New York Mets')
+			expect(teamId).to.equal('121');
+		});
+		it ('should find and return the correct team ID when given a team ID', function () {
+			let teamId = find.teamId('121');
+			expect(teamId).to.equal('121');
+		});
+	});
+
+});
+
