0cea187c4274e607fb3240d776cb5772530f9610
Chris Apparicio <chris@apparicio.me>
2017-04-30 14:07:12 +0100

    pruning for player stats

--- utils/prune/index.js
+++ utils/prune/index.js
@@ -67,6 +67,19 @@ prune.prototype.playerData = function (data) {
 	return player;
 }
 
+prune.prototype.playerStats = function (data) {
+	let prunedData;
+	let isHittingStats = data.hasOwnProperty('sport_hitting_tm');
+
+	if (isHittingStats) {
+		prunedData  = data.sport_hitting_tm.queryResults.row;
+	} else {
+		prunedData = data.sport_pitching_tm.queryResults.row;
+	}
+	
+	return prunedData;
+}
+
 prune.prototype.rosterData = function (data) {
 	let prunedData = [];
 	let roster = data.roster_all.queryResults.row;
