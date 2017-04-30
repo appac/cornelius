eea24369f00e30aba503caf9134d5c9aa40b3969
Chris Apparicio <chris@apparicio.me>
2017-04-30 14:08:38 +0100

    hitting and pitching stats calls

--- utils/mlb/index.js
+++ utils/mlb/index.js
@@ -107,4 +107,103 @@ mlb.prototype.roster = function (teamId) {
 	});
 }
 
+mlb.prototype.hittingStats = function (playerId, year) {
+	return new Promise (function (resolve, reject) {
+		if (!year) {
+			year = '2017'
+		}
+
+		let uri = url.parse(baseUrl + `/named.sport_hitting_tm.bam?game_type=%27R%27&league_list_id=%27mlb%27&player_id=${playerId}&sport_hitting_tm.season=${year}`);
+
+		let reqOptions = {
+			host: uri.host,
+			path: uri.path
+		}
+		http.get(reqOptions, function (res) {
+			const statusCode = res.statusCode;
+			const contentType = res.headers['content-type'];
+
+			let error;
+
+			if (statusCode !== 200) {
+				error = new Error(`${statusCode} - Request failed.`);
+			} else if (!/^application\/json/.test(contentType)) {
+				error = new Error(`Invalid content type received. Expected JSON, got ${contentType}`);
+			}
+
+			if (error) {
+				res.resume();
+				reject(error);
+			}
+
+			res.setEncoding('utf8');
+
+			let rawData = '';
+			res.on('data', (chunk) => {
+				rawData += chunk;
+			});
+			res.on('end', () => {
+				try {
+					const parsedData = JSON.parse(rawData);
+					resolve(parsedData);
+				} catch (e) {
+					reject(e)
+				}
+			});
+		}).on('error', (e) => {
+			reject(e);
+		});
+	});
+}
+
+mlb.prototype.pitchingStats = function (playerId, year) {
+	//mlb.mlb.com/lookup/json/named.sport_pitching_tm.bam?game_type=%27R%27&league_list_id=%27mlb%27&player_id=519242&sport_pitching_tm.season=2016
+	return new Promise (function (resolve, reject) {
+		if (!year) {
+			year = '2017'
+		}
+
+		let uri = url.parse(baseUrl + `/named.sport_pitching_tm.bam?game_type=%27R%27&league_list_id=%27mlb%27&player_id=${playerId}&sport_pitching_tm.season=${year}`);
+
+		let reqOptions = {
+			host: uri.host,
+			path: uri.path
+		}
+		http.get(reqOptions, function (res) {
+			const statusCode = res.statusCode;
+			const contentType = res.headers['content-type'];
+
+			let error;
+
+			if (statusCode !== 200) {
+				error = new Error(`${statusCode} - Request failed.`);
+			} else if (!/^application\/json/.test(contentType)) {
+				error = new Error(`Invalid content type received. Expected JSON, got ${contentType}`);
+			}
+
+			if (error) {
+				res.resume();
+				reject(error);
+			}
+
+			res.setEncoding('utf8');
+
+			let rawData = '';
+			res.on('data', (chunk) => {
+				rawData += chunk;
+			});
+			res.on('end', () => {
+				try {
+					const parsedData = JSON.parse(rawData);
+					resolve(parsedData);
+				} catch (e) {
+					reject(e)
+				}
+			});
+		}).on('error', (e) => {
+			reject(e);
+		});
+	});
+}
+
 module.exports = new mlb;
