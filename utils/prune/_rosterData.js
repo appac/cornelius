445dd8a7e845013630b53e0b4a6900acebe249a1
Chris Apparicio <chris@apparicio.me>
2017-05-02 17:55:57 +0100

    roster pruning now handles full or short form player data

--- utils/prune/_rosterData.js
+++ utils/prune/_rosterData.js
@@ -1,15 +1,25 @@
+let prunePlayerData = require('./_playerData');
+
 function pruneRosterData (data) {
 	let prunedData = [];
 	let roster = data.roster_all.queryResults.row;
-	// TODO
-	// add check for full roster data, prune accordingly
-	roster.forEach(function (player) {
-		let prunedPlayer = {
-			id: player.player_id,
-			name: player.name_display_first_last
+	let hasFullPlayerData = roster[0].hasOwnProperty('pro_debut_date');
+	if (hasFullPlayerData) {
+		for (let i = 0; i < roster.length; i++) {
+			let player = roster[i];
+			let prunedPlayer = prunePlayerData(player);
+			prunedData.push(prunedPlayer);
 		}
-		prunedData.push(prunedPlayer);
-	});
+	} else {
+		for (let i = 0; i < roster.length; i++) {
+			let player = roster[i];
+			let prunedPlayer = {
+				id: player.player_id,
+				name: player.name_display_first_last
+			}
+			prunedData.push(prunedPlayer);
+		}
+	}
 
 	return prunedData;
 }
