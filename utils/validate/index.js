5316cae5a054830ed939c3142f4ddbfd0f864c70
Chris Apparicio <chris@apparicio.me>
2017-10-23 22:45:50 +0100

    remove old validator files

--- utils/validate/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-    searchPlayer: require('./_searchOptions'),
-    getPlayer: require('./_getOptions'),
-    getRoster: require('./_rosterOptions'),
-    getStats: require('./_statsOptions')
-};
