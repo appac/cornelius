ce27c0a60dde135005247bbcebb63b6fa7e161a4
Chris Apparicio <chris@apparicio.me>
2017-12-07 22:17:04 +0000

    remove pruneData method
    pruning is now handled via options passed to a method, and is no longer exposed directly to users

--- cornelius.js
+++ cornelius.js
@@ -6,6 +6,5 @@ Cornelius.prototype.searchPlayer = require('./lib/searchPlayer');
 Cornelius.prototype.getPlayer = require('./lib/getPlayer');
 Cornelius.prototype.getStats = require('./lib/getStats');
 Cornelius.prototype.getRoster = require('./lib/getRoster');
-Cornelius.prototype.pruneData = require('./lib/pruneData');
 
 module.exports = new Cornelius();
