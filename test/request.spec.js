50fdb6082a70eb785d6d4ee0023ad06cb7fd7448
Chris Apparicio <chris@apparicio.me>
2017-12-08 17:14:45 +0000

    update tests to reflect new required options

--- test/request.spec.js
+++ test/request.spec.js
@@ -18,9 +18,16 @@ test('buildRequest should return undefined when given invalid query type',
 test('buildRequest(search_player_all) should return a valid search url',
     (t) => {
         t.plan(1);
-        const url = mlbReq.build('search_player_all', 'someTestQuery');
-        const expectedUrl = 'http://mlb.mlb.com/lookup/json/named.search_player_all.bam?sport_code=%27mlb%27&name_part=%27someTestQuery%25%27&active_sw=%27Y%27';
+        let url;
+        let  expectedUrl;
 
+        t.comment('Active player search.');
+        url = mlbReq.build('search_player_all', { query: 'someTestQuery3579', active: true });
+        expectedUrl = 'http://mlb.mlb.com/lookup/json/named.search_player_all.bam?sport_code=%27mlb%27&name_part=%27someTestQuery3579%25%27&active_sw=%27Y%27';
+        t.equal(
+            url.href, expectedUrl,
+            'Actual search_player_all url does not match expected url.'
+        );
         t.equal(
             url.href, expectedUrl,
             'Actual search_player_all url does not match expected url.'
@@ -31,8 +38,8 @@ test('buildRequest(search_player_all) should return a valid search url',
 test('buildRequest(player_info) should return a valid player info url',
     (t) => {
         t.plan(1);
-        const url = mlbReq.build('player_info', '12345');
-        const expectedUrl = 'http://mlb.mlb.com/lookup/json/named.player_info.bam?sport_code=%27mlb%27&player_id=%2712345%27';
+        const url = mlbReq.build('player_info', { player_id: '3579' });
+        const expectedUrl = 'http://mlb.mlb.com/lookup/json/named.player_info.bam?sport_code=%27mlb%27&player_id=%273579%27';
 
         t.equal(
             url.href, expectedUrl,
@@ -44,8 +51,8 @@ test('buildRequest(player_info) should return a valid player info url',
 test('buildRequest(sport_hitting_tm) should return a valid hitting stats url',
     (t) => {
         t.plan(1);
-        const url = mlbReq.build('sport_hitting_tm', '12345');
-        const expectedUrl = 'http://mlb.mlb.com/lookup/json/named.sport_hitting_tm.bam?player_id=12345&game_type=%27R%27&league_list_id=%27mlb%27';
+        const url = mlbReq.build('sport_hitting_tm', { player_id: '3579' });
+        const expectedUrl = 'http://mlb.mlb.com/lookup/json/named.sport_hitting_tm.bam?player_id=3579&game_type=%27R%27&league_list_id=%27mlb%27';
 
         t.equal(
             url.href, expectedUrl,
@@ -57,8 +64,8 @@ test('buildRequest(sport_hitting_tm) should return a valid hitting stats url',
 test('buildRequest(sport_pitching_tm) should return a valid pitching stats url',
     (t) => {
         t.plan(1);
-        const url = mlbReq.build('sport_pitching_tm', '12345');
-        const expectedUrl = 'http://mlb.mlb.com/lookup/json/named.sport_pitching_tm.bam?player_id=12345&game_type=%27R%27&league_list_id=%27mlb%27';
+        const url = mlbReq.build('sport_pitching_tm', { player_id: '3579', pitching: true });
+        const expectedUrl = 'http://mlb.mlb.com/lookup/json/named.sport_pitching_tm.bam?player_id=3579&game_type=%27R%27&league_list_id=%27mlb%27';
 
         t.equal(
             url.href, expectedUrl,
@@ -70,9 +77,16 @@ test('buildRequest(sport_pitching_tm) should return a valid pitching stats url',
 test('buildRequest(roster_40) should return a valid roster url',
     (t) => {
         t.plan(1);
-        const url = mlbReq.build('roster_40', '123');
-        const expectedUrl = 'http://mlb.mlb.com/lookup/json/named.roster_40.bam?team_id=%27123%27&roster_40.col_in=name_display_first_last&roster_40.col_in=player_id';
+        let url;
+        let expectedUrl;
 
+        t.comment('Long form player data.');
+        url = mlbReq.build('roster_40', { team_id: '3579' });
+        expectedUrl = 'http://mlb.mlb.com/lookup/json/named.roster_40.bam?team_id=%273579%27';
+        t.equal(
+            url.href, expectedUrl,
+            'Actual roster_40 url does not match expected url.'
+        );
         t.equal(
             url.href, expectedUrl,
             'Actual roster_40 url does not match expected url.'
